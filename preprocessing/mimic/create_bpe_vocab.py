419b4b1ff0ea859aa2e5d29de97494f8ac8e5429
Roghiehghahremani <143075444+Roghiehghahremani@users.noreply.github.com>
2025-03-13 15:51:19 +0330

    Update create_bpe_vocab.py

--- preprocessing/mimic/create_bpe_vocab.py
+++ preprocessing/mimic/create_bpe_vocab.py
@@ -1,13 +1,20 @@
-import pandas as pd
+import pandas as pd # pandas is used to read and process the dataset (mimic_cxr_labeled.csv).
 
-from tokenizers import ByteLevelBPETokenizer
+"""yteLevelBPETokenizer is a subword tokenizer from the tokenizers library, which efficiently handles rare and 
+frequent words in text data."""
+from tokenizers import ByteLevelBPETokenizer 
 
+"""Reads the MIMIC-CXR labeled dataset from a CSV file.
+Drops missing/empty reports to ensure only valid text data is processed.
+Converts the reports into a Python list (reports), which will be used for training the tokenizer."""
 MIMIC_REPORTS = './mimic_cxr_labeled.csv'
 
 reports = pd.read_csv(MIMIC_REPORTS)
 reports = reports.dropna(subset=['Reports'])  # delete empty reports
 reports = list(reports['Reports'].values)
 
+"""Writes all the reports line by line to /tmp/mimic.txt.
+This file serves as input for training the tokenizer."""
 with open('/tmp/mimic.txt', 'w') as f:
     for item in reports:
         f.write("%s\n" % item)
